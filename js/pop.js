e11949220b843b376f6e19480902972f504d8760
Sharon <sharontam1024@gmail.com>
2016-02-01 12:30:01 -0800

    yeahhhhhhhhhhh

--- js/pop.js
+++ js/pop.js
@@ -1,4 +1,5 @@
  $(document).ready(function () {
+
      $("#reg1").click(function () {
          $(".pop").fadeIn(300);
 
@@ -15,8 +16,98 @@
          $(".pop2").fadeIn(300);
          //positionPopup();
      });
-
+	  
 $(".pop2 > span, .pop2").click(function (event) {
     event.stopPropagation();
     $(".pop2").fadeOut(300);
+     });
+	 
+	 $("#reg3").click(function () {
+         $(".pop3").fadeIn(300);
+         //positionPopup();
+     });
+	  
+$(".pop3 > span, .pop3").click(function (event) {
+    event.stopPropagation();
+    $(".pop3").fadeOut(300);
+     });
+	 
+	 	 $("#reg4").click(function () {
+         $(".pop4").fadeIn(300);
+         //positionPopup();
+     });
+	  
+$(".pop4 > span, .pop4").click(function (event) {
+    event.stopPropagation();
+    $(".pop4").fadeOut(300);
+     });
+	 
+	 	 $("#reg5").click(function () {
+         $(".pop5").fadeIn(300);
+         //positionPopup();
+     });
+	  
+$(".pop5 > span, .pop5").click(function (event) {
+    event.stopPropagation();
+    $(".pop5").fadeOut(300);
+     });
+	 
+	 	 $("#reg6").click(function () {
+         $(".pop6").fadeIn(300);
+         //positionPopup();
+     });
+	  
+$(".pop6 > span, .pop6").click(function (event) {
+    event.stopPropagation();
+    $(".pop6").fadeOut(300);
+     });
+	 
+	 	 $("#reg7").click(function () {
+         $(".pop7").fadeIn(300);
+         //positionPopup();
+     });
+	  
+$(".pop7 > span, .pop7").click(function (event) {
+    event.stopPropagation();
+    $(".pop7").fadeOut(300);
+     });
+	 
+	 	 $("#reg8").click(function () {
+         $(".pop8").fadeIn(300);
+         //positionPopup();
+     });
+	  
+$(".pop8 > span, .pop8").click(function (event) {
+    event.stopPropagation();
+    $(".pop8").fadeOut(300);
+     });
+	 
+	 	 $("#reg9").click(function () {
+         $(".pop9").fadeIn(300);
+         //positionPopup();
+     });
+	  
+$(".pop9 > span, .pop9").click(function (event) {
+    event.stopPropagation();
+    $(".pop9").fadeOut(300);
+     });
+	 
+	 	 $("#reg10").click(function () {
+         $(".pop10").fadeIn(300);
+         //positionPopup();
+     });
+	  
+$(".pop10 > span, .pop10").click(function (event) {
+    event.stopPropagation();
+    $(".pop10").fadeOut(300);
+     });
+	 
+	 	 $("#reg11").click(function () {
+         $(".pop11").fadeIn(300);
+         //positionPopup();
+     });
+	  
+$(".pop11 > span, .pop11").click(function (event) {
+    event.stopPropagation();
+    $(".pop11").fadeOut(300);
      });
