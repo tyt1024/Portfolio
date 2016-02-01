d96da7f7c97e7efaa08bd3b15f72088a4b2335b5
kay8 <kay.yamagishi.work@gmail.com>
2016-01-31 17:33:19 -0800

    fix pop-up
    - gallery css: change popup position
    - js: comment out undefined function and class name
    - html: add classes

--- js/pop.js
+++ js/pop.js
@@ -1,19 +1,22 @@
  $(document).ready(function () {
      $("#reg1").click(function () {
          $(".pop").fadeIn(300);
-         positionPopup();
+
+         //positionPopup();
      });
 
-     $(".pop > span, .pop").click(function () {
+     $(".pop-close").click(function (event) {
+        event.stopPropagation();
          $(".pop").fadeOut(300);
      });
  });
 
      $("#reg2").click(function () {
          $(".pop2").fadeIn(300);
-         positionPopup();
+         //positionPopup();
      });
 
-$(".pop2 > span, .pop2").click(function () {
-         $(".pop2").fadeOut(300);
+$(".pop2 > span, .pop2").click(function (event) {
+    event.stopPropagation();
+    $(".pop2").fadeOut(300);
      });
